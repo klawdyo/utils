23ae0adcd531589df4c639e4c0b42ba68f8ad75e
Cláudio Medeiros <klawdyo@gmail.com>
2022-02-23 20:07:40 -0300

    test: Acréscimo de um teste em sortByKey para melhorar o coverage

--- src/main.test.ts
+++ src/main.test.ts
@@ -517,6 +517,7 @@ test("sortByKey", () => {
     { age: "20", name: "claudio" },
     { age: "25", name: "marta" },
     { age: "10", name: "isa" },
+    { age: "15", name: "isa" },
   ];
 
   //
@@ -524,12 +525,14 @@ test("sortByKey", () => {
 
   expect(arr1[0]).toMatchObject({ age: "20", name: "claudio" });
   expect(arr1[1]).toMatchObject({ age: "10", name: "isa" });
-  expect(arr1[2]).toMatchObject({ age: "25", name: "marta" });
+  expect(arr1[2]).toMatchObject({ age: "15", name: "isa" });
+  expect(arr1[3]).toMatchObject({ age: "25", name: "marta" });
 
   //
   const arr2 = sortByKey(list, "age");
 
   expect(arr2[0]).toMatchObject({ age: "10", name: "isa" });
-  expect(arr2[1]).toMatchObject({ age: "20", name: "claudio" });
-  expect(arr2[2]).toMatchObject({ age: "25", name: "marta" });
+  expect(arr2[1]).toMatchObject({ age: "15", name: "isa" });
+  expect(arr2[2]).toMatchObject({ age: "20", name: "claudio" });
+  expect(arr2[3]).toMatchObject({ age: "25", name: "marta" });
 });
